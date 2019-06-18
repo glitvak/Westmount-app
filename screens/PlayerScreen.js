14d625ac89e1117fa0aadfa92532dfb0f067b764
Gil <gillitvak@gmail.com>
2019-06-18 15:16:23 -0400

    Audio Playback working
    Next is to complete UI & functionality of seeking/stoping when leaving the screen/etc

--- screens/PlayerScreen.js
+++ screens/PlayerScreen.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {View, Text, ScrollView, ActivityIndicator, StyleSheet, Dimensions,Button} from 'react-native';
 import { MonoText } from '../components/StyledText';
 import Header from '../components/SubHeader';
-import SoundPlayer from 'react-native-sound-player';
+import { Slider } from 'react-native-elements';
+import { Audio } from 'expo';
 
 export default class PlayerScreen extends React.Component {
   static navigationOptions = {
@@ -10,30 +11,71 @@ export default class PlayerScreen extends React.Component {
     TabBarVisible: false,
   };
   state ={
+    classAudio: null,
     isPlaying: false,
     isLoadingComplete: false
   }
 
-  play(){
+  componentDidMount(){
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      playThroughEarpieceAndroid: false,
+    });
+    const soundObject = new Audio.Sound();
+    soundObject.loadAsync({uri: 'http://westmountshul.com/wp-content/uploads/Parsha-June-11-2019.mp3'});
+    this.setState({
+      classAudio: soundObject,
+      isPlaying: false,
+      isLoadingComplete: true
+    })
+  }
+
+  async play(){
     try {
-      SoundPlayer.playUrl('http://westmountshul.com/wp-content/uploads/Parsha-June-11-2019.mp3');
+      await this.state.classAudio.playAsync();
+      // Your sound is playing!
+      console.log(this.state.classAudio.PlaybackStatusToSet)
+    } catch (error) {
+      console.log(error);
     }
-    catch (e)
-    {
-        console.log(`cannot play the sound file`, e);
+  }
+
+  async stop(){
+    try{
+      await this.state.classAudio.pauseAsync();
+    }
+    catch(error){
+      console.log(error);
     }
   }
 
   render(){
-    return (
-      <View style={styles.container}>
-        <Header navigation={this.props.navigation}/>
-        <View>
-          <Text>Testing Player Screen</Text>
-          <Button onPress={()=>this.play()} title={'test sound'}/>
+    if (this.state.isLoadingComplete) {
+      return (
+        <View style={styles.container}>
+          <Header navigation={this.props.navigation}/>
+          <View>
+            <Text>Testing Player Screen</Text>
+            <Button onPress={()=>this.play()} title={'test sound'}/>
+            <Button onPress={()=>this.stop()} title={'Stop sound'}/>
+          </View>
         </View>
-      </View>
-    );
+      );
+    }
+    else {
+      return(
+        <View style={styles.container}>
+          <Header navigation={this.props.navigation}/>
+          <View>
+          <Text>Loading!</Text>
+          </View>
+        </View>
+      );
+    }
   }
 }
 
